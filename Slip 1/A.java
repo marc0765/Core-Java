7425bdf172d7d364dbe7a3c80c5c6311e51e9a47
Maria Rodricks <124487023+marc0765@users.noreply.github.com>
2023-03-10 21:47:03 +0530

    Solution to Slip 1(A)
    Java program to display characters from ‘A’ to ‘Z’

---
+++ Slip 1/A.java
@@ -0,0 +1,12 @@
+// Write a ‘java’ program to display characters from ‘A’ to ‘Z’
+
+class Slip1A
+{
+    public static void main(String[] args)
+    {
+        for(char i='A'; i<='Z'; i++)
+        {
+            System.out.print(i+" ");
+        }
+    }
+}
